a3ea07b8e515eb81514ddf6250916a3d4dca1fae
Belyj <Belyj_07@inbox.ru>
2017-01-05 00:51:02 +0400

    Начали C# черепашку делать

---
+++ HelloWorld/FilmChoose/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FilmChoose
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Как Вас зовут?");
+            String name = Console.ReadLine();
+            
+            Console.WriteLine("Здравствуйте, " + name + ". Давайте выберем фильм на вечер.");
+            Console.WriteLine("Какой жанр фильма Вы предпочитаете, комедия или драмма?");
+            String genre = Console.ReadLine();
+
+            if (genre == "комедия")
+            {
+                Console.WriteLine("1 - Час пик");
+                Console.WriteLine("2 - Плохие парни");
+                Console.WriteLine("3 - Вышибала");
+            } else
+            {
+                Console.WriteLine("1 - Побег из Шоушенка");
+                Console.WriteLine("2 - Вечное сияние чистоо разума");
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ HelloWorld/TurtleTraining/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.SmallBasic.Library;
+
+namespace TurtleTraining
+{
+    class Program
+    {
+        static void WriteT(int size)
+        {
+            Turtle.Move(size*2);
+            Turtle.Angle = 270;
+            Turtle.Move (size);
+            Turtle.Angle = 90;
+            Turtle.Move (size*2);
+        }
+
+        static void WriteO()
+        {
+            for (int i = 0; i < 2; i++)
+            {
+                Turtle.Move(70);
+                Turtle.TurnRight();
+                Turtle.Move(40);
+                Turtle.TurnRight();
+            }
+        }
+
+        static void WriteP()
+        {
+            Turtle.Move(70);
+            for (int i = 0; i < 3; i++)
+            {
+                Turtle.TurnRight();
+                Turtle.Move(40);
+            }
+
+        }
+        static void Main(string[] args)
+        {
+            Turtle.X = 150;
+            Turtle.Y = 200;
+            WriteT(50);
+            Turtle.X = 200;
+            Turtle.Y = 200;
+            Turtle.Angle = 0;
+            WriteO();
+            Turtle.X = 270;
+            Turtle.Y = 200;
+            WriteP();
+            Turtle.X = 370;
+            Turtle.Y = 200;
+            Turtle.Angle = 0;
+            WriteT(35);
+        }
+    }
+}
